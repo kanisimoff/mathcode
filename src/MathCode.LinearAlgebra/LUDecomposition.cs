9c86866eabf593fa6476d783adf06e3c3db89b3a
M@ster Konst <konst@itmasters.pro>
2022-06-19 13:27:28 +0300

    update libraries

---
+++ src/MathCode.LinearAlgebra/LUDecomposition.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathCode.LinearAlgebra
+{
+    internal class LUDecomposition
+    {
+    }
+}
